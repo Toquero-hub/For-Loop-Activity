e5dd37715f8e5221e59b72072ffb5f8d4d80ad5f
Toquero-hub <toquerojosh48@gmail.com>
2024-10-01 15:13:58 +0800

    Add files via upload

---
+++ evenNumber.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+// Loop through numbers from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    // Check if the number is even
+    if (i % 2 === 0) {
+    // Log the even number to the console
+    console.log(i);
+    }
+    }
